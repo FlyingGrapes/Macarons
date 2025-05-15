bc0c318ff9c4241b90a2b749de1528b778c187ce
Grace <gracengxy2021@gmail.com>
2025-05-15 22:04:45 +0800

    file edits

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 
 
-# node.js ()
-## npm init -y
-## npm install express
+#### node.js ()
+#### npm init -y
+#### npm install express
+#### References:
+#### [text](https://www.youtube.com/watch?v=bpjykKvvvm0&list=TLPQMTQwNTIwMjU6G0nDijjqsg&index=3)
+#### stopped at 13:45
--- ePortFolio/js/script.js
+++ ePortFolio/js/script.js
@@ -1,1 +1,32 @@
-console.log("✅ script.js is working!");
+import Swiper, { Navigation, Pagination, Scrollbar } from 'swiper';
+
+// import swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+// configure modules you want to use
+Swiper.use([Navigation, Pagination, Scrollbar]);
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  slidesPerView: 1,
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
